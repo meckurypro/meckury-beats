7640294341858e407082975ba4bac5d82412a27c
meckurypro <meckurypro@gmail.com>
2026-01-09 11:01:38 +0100

    Update page.tsx

--- app/beats/[slug]/page.tsx
+++ app/beats/[slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
